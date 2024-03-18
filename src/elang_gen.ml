243e92d8c8adb139862f9eecfd7e98a3055d890b
womax <antonin.ruan@student-cs.fr>
2024-03-18 16:10:41 +0100

    E generator

--- src/elang_gen.ml
+++ src/elang_gen.ml
@@ -45,8 +45,12 @@ let rec make_eexpr_of_ast (a: tree) : expr res =
   let res =
     match a with
     | Node(t, [e1; e2]) when tag_is_binop t ->
-         Error (Printf.sprintf "Unacceptable ast in make_eexpr_of_ast %s"
-                        (string_of_ast a))
+        let lsubtree = error_fail (make_eexpr_of_ast e1) identity and rsubtree = error_fail (make_eexpr_of_ast e2) identity in
+        OK (Ebinop (binop_of_tag t, lsubtree, rsubtree))
+    | Node(t, [e1]) when t = Tneg -> let subtree = error_fail (make_eexpr_of_ast e1) identity in
+        OK (Eunop (Eneg, subtree))
+    | Node(t, [IntLeaf x]) when t = Tint -> OK (Eint x)
+    | Node(t, [StringLeaf s]) when t = Tvar -> OK(Evar s)
     | _ -> Error (Printf.sprintf "Unacceptable ast in make_eexpr_of_ast %s"
                     (string_of_ast a))
   in
@@ -58,7 +62,31 @@ let rec make_eexpr_of_ast (a: tree) : expr res =
 let rec make_einstr_of_ast (a: tree) : instr res =
   let res =
     match a with
-    (* TODO *)
+    | Node(t, list) when t = Tblock -> let subtree_list = List.map (fun elt -> make_einstr_of_ast elt) list in
+        let instr_list = List.map (fun elt -> error_fail elt identity) subtree_list in
+        OK (Iblock instr_list)
+    | Node(t, subtree) when t = Tif -> (
+      match subtree with
+      | [cnd; if_block; else_block] -> let ecnd_res = make_eexpr_of_ast cnd 
+                                        and if_instr_res = make_einstr_of_ast if_block 
+                                        and else_instr_res = make_einstr_of_ast else_block
+        in let ecnd = error_fail ecnd_res identity and if_instr = error_fail if_instr_res identity and else_instr = error_fail else_instr_res identity
+        in OK (Iif (ecnd, if_instr, else_instr))
+      | [cnd; if_block] -> let ecnd_res = make_eexpr_of_ast cnd and if_instr_res = make_einstr_of_ast if_block 
+        in let ecnd = error_fail ecnd_res identity and if_instr = error_fail if_instr_res identity
+        in OK(Iif(ecnd , if_instr, Iblock []))
+      | _ -> Error "" 
+    )
+    | Node(t, [cnd; instrs]) when t = Twhile -> let icnd_res = make_eexpr_of_ast cnd and instrs = make_einstr_of_ast instrs
+      in let icnd = error_fail icnd_res identity and instrs = error_fail instrs identity in
+      OK (Iwhile (icnd, instrs))
+    | Node(t, [expr]) when t = Treturn -> let eexpr = error_fail (make_eexpr_of_ast expr) identity in OK(Ireturn eexpr)
+    | Node(t, [expr]) when t = Tprint -> let eexpr = error_fail (make_eexpr_of_ast expr) identity in OK(Iprint eexpr)
+    | Node(t, [id; expr]) when t = Tassign -> let eid = error_fail (make_eexpr_of_ast id) identity and eexpr = error_fail (make_eexpr_of_ast expr) identity
+      in (match eid with 
+        | (Evar str) -> OK (Iassign (str, eexpr))
+        | _ -> Error (Printf.sprintf "Unacceptable ast in make_einstr_of_ast %s. Left subtree must be of type Tvar when in Tassign" (string_of_ast a))
+      )
     | _ -> Error (Printf.sprintf "Unacceptable ast in make_einstr_of_ast %s"
                     (string_of_ast a))
   in
@@ -76,10 +104,10 @@ let make_ident (a: tree) : string res =
 
 let make_fundef_of_ast (a: tree) : (string * efun) res =
   match a with
-  | Node (Tfundef, [StringLeaf fname; Node (Tfunargs, fargs); fbody]) ->
+  | Node (Tfundef, [Node(Tfunname, [StringLeaf fname]); Node (Tfunargs, fargs); Node(Tfunbody, [fbody])]) ->
     list_map_res make_ident fargs >>= fun fargs ->
-     (* TODO *)
-     Error "make_fundef_of_ast: Not implemented, yet."
+     let funbody = (make_einstr_of_ast fbody) >>! identity in
+     OK (fname, {funargs = fargs; funbody = funbody})
   | _ ->
     Error (Printf.sprintf "make_fundef_of_ast: Expected a Tfundef, got %s."
              (string_of_ast a))
